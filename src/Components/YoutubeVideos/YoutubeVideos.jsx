30b4661c0e0e27d857565750150afafcd2daabff
Silvain <silver383@gmail.com>
2021-08-03 18:30:57 +0300

    status

--- src/Components/YoutubeVideos/YoutubeVideos.jsx
+++ src/Components/YoutubeVideos/YoutubeVideos.jsx
@@ -1,16 +1,22 @@
 import YoutubeVideo from "./YoutubeVideo/YoutubeVideo";
 
 const videosData = [
-	{ id: "976LGx9NP0A", title: "Allsa" },
-	{ id: "r-VXB9LRMoo", title: "Ne zalishaj" },
-	{ id: "tgL4BUJAgEo", title: "Kintseva zupynka" },
-	{ id: "5MkNL1S75fU", title: "Ot sebyz" },
-	{ id: "SQkI6b4tBYU", title: "Z toboyu" },
+	{ id: "976LGx9NP0A", title: "Allsa", status: "video", idStatus: 0 },
+	{ id: "r-VXB9LRMoo", title: "Ne zalishaj", status: "img", idStatus: 1 },
+	{ id: "tgL4BUJAgEo", title: "Kintseva zupynka", status: "img", idStatus: 2 },
+	{ id: "5MkNL1S75fU", title: "Ot sebyz", status: "img", idStatus: 3 },
+	{ id: "SQkI6b4tBYU", title: "Z toboyu", status: "img", idStatus: 4 },
 ]
+
+const changeStatus = (idStatus) => {
+	videosData[idStatus].status = "video";
+	
+}
 
 
 const YoutubeVideos = () => {
-	return videosData.map((v) => { return <YoutubeVideo id={v.id} /> });
+
+	return videosData.map((v) => { return <YoutubeVideo idStatus={v.idStatus} id={v.id} title={v.title} status={v.status} changeStatus={changeStatus} /> });
 }
 
 
