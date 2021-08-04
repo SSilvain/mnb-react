8aebd9f04e51502fc1f117e75b04c2bef1cebb62
Silvain <silver383@gmail.com>
2021-08-04 23:52:09 +0300

    redux

--- src/Components/YoutubeVideos/YoutubeVideos.jsx
+++ src/Components/YoutubeVideos/YoutubeVideos.jsx
@@ -1,22 +1,24 @@
+import { useDispatch, useSelector } from "react-redux";
 import YoutubeVideo from "./YoutubeVideo/YoutubeVideo";
 
-const videosData = [
-	{ id: "976LGx9NP0A", title: "Allsa", status: "video", idStatus: 0 },
-	{ id: "r-VXB9LRMoo", title: "Ne zalishaj", status: "img", idStatus: 1 },
-	{ id: "tgL4BUJAgEo", title: "Kintseva zupynka", status: "img", idStatus: 2 },
-	{ id: "5MkNL1S75fU", title: "Ot sebyz", status: "img", idStatus: 3 },
-	{ id: "SQkI6b4tBYU", title: "Z toboyu", status: "img", idStatus: 4 },
-]
-
-const changeStatus = (idStatus) => {
-	videosData[idStatus].status = "video";
-	
-}
-
-
 const YoutubeVideos = () => {
+	
 
-	return videosData.map((v) => { return <YoutubeVideo idStatus={v.idStatus} id={v.id} title={v.title} status={v.status} changeStatus={changeStatus} /> });
+	let dispatch = useDispatch();
+	let videos = useSelector(state => state.videos);
+
+	const changeStatus = (idStatus) => {
+		
+			
+		videosNew = [...videos, videos[action.idStatus].status = "video"];
+		
+		dispatch({ type: "VIDEO", videosNew: videosNew });
+		console.log("ok");
+
+
+	}
+
+	return videos.map((v) => { return <YoutubeVideo idStatus={v.idStatus} id={v.id} title={v.title} status={v.status} changeStatus={changeStatus} /> });
 }
 
 
--- src/index.js
+++ src/index.js
@@ -1,73 +1,106 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
+let initialState = {
+    status: "IMG",
+    videos: [
+        { id: "976LGx9NP0A", title: "Allsa", status: "img", idStatus: 0 },
+        { id: "r-VXB9LRMoo", title: "Ne zalishaj", status: "img", idStatus: 1 },
+        {
+            id: "tgL4BUJAgEo",
+            title: "Kintseva zupynka",
+            status: "img",
+            idStatus: 2,
+        },
+        { id: "5MkNL1S75fU", title: "Ot sebyz", status: "img", idStatus: 3 },
+        { id: "SQkI6b4tBYU", title: "Z toboyu", status: "img", idStatus: 4 },
+    ],
+};
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case "VIDEO":
+            return {
+                ...state,
+                videos: action.videosNew
+            };
+        default:
+            return state;
+    }
+};
+
+let store = createStore(reducer);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <BrowserRouter>
+            <Provider store={store}>
+                <App />
+            </Provider>
+        </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById("root")
 );
 
+// function findVideos() {
+//     let videos = document.querySelectorAll(".video");
 
-	function findVideos() {
-        let videos = document.querySelectorAll(".video");
+//     for (let i = 0; i < videos.length; i++) {
+//         setupVideo(videos[i]);
+//     }
+// }
 
-        for (let i = 0; i < videos.length; i++) {
-            setupVideo(videos[i]);
-        }
-    }
+// function setupVideo(video) {
+//     let link = video.querySelector(".video__link");
+//     let media = video.querySelector(".video__media");
+//     let button = video.querySelector(".video__button");
+//     let id = parseMediaURL(media);
 
-    function setupVideo(video) {
-        let link = video.querySelector(".video__link");
-        let media = video.querySelector(".video__media");
-        let button = video.querySelector(".video__button");
-        let id = parseMediaURL(media);
+//     video.addEventListener("click", () => {
+//         let iframe = createIframe(id);
 
-        video.addEventListener("click", () => {
-            let iframe = createIframe(id);
+//         link.remove();
+//         button.remove();
+//         video.appendChild(iframe);
+//     });
 
-            link.remove();
-            button.remove();
-            video.appendChild(iframe);
-        });
+//     link.removeAttribute("href");
+//     video.classList.add("video--enabled");
+// }
 
-        link.removeAttribute("href");
-        video.classList.add("video--enabled");
-    }
+// function parseMediaURL(media) {
+//     let regexp =
+//         /https:\/\/i\.ytimg\.com\/vi\/([a-zA-Z0-9_-]+)\/maxresdefault\.jpg/i;
+//     let url = media.src;
+//     let match = url.match(regexp);
 
-    function parseMediaURL(media) {
-        let regexp =
-            /https:\/\/i\.ytimg\.com\/vi\/([a-zA-Z0-9_-]+)\/maxresdefault\.jpg/i;
-        let url = media.src;
-        let match = url.match(regexp);
+//     return match[1];
+// }
 
-        return match[1];
-    }
+// function createIframe(id) {
+//     let iframe = document.createElement("iframe");
 
-    function createIframe(id) {
-        let iframe = document.createElement("iframe");
+//     iframe.setAttribute("allowfullscreen", "");
+//     iframe.setAttribute("allow", "autoplay");
+//     iframe.setAttribute("src", generateURL(id));
+//     iframe.classList.add("video__media");
 
-        iframe.setAttribute("allowfullscreen", "");
-        iframe.setAttribute("allow", "autoplay");
-        iframe.setAttribute("src", generateURL(id));
-        iframe.classList.add("video__media");
+//     return iframe;
+// }
 
-        return iframe;
-    }
+// function generateURL(id) {
+//     let query = "?rel=0&showinfo=0&autoplay=1";
 
-    function generateURL(id) {
-        let query = "?rel=0&showinfo=0&autoplay=1";
+//     return "https://www.youtube.com/embed/" + id + query;
+// }
 
-        return "https://www.youtube.com/embed/" + id + query;
-    }
-
-    // findVideos();
+// findVideos();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
