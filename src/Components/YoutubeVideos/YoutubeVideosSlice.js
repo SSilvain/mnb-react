cdf431aac9e29fe9918d9f4db2822f09183f01bc
Silvain <silver383@gmail.com>
2021-08-08 19:33:14 +0300

    test

--- src/Components/YoutubeVideos/YoutubeVideosSlice.js
+++ src/Components/YoutubeVideos/YoutubeVideosSlice.js
@@ -45,7 +45,7 @@ export const slice = createSlice({
         statusvideo: (state, action) => {
             // state.videos[action.payload].status = "video";
             
-            state.videos = state.videos.filter((v, index) => { if (index === action.payload) { v.status = "video" } return true });
+
             return state; 
         },
     },
